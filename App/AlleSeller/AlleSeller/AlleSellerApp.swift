df3267532b87fe3220dc10a0536e3df3446d8ea2
Lukasz Stocki <stocki.lukasz@gmail.com>
2023-08-12 14:49:20 +0200

    Adds empty project.

---
+++ App/AlleSeller/AlleSeller/AlleSellerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlleSellerApp.swift
+//  AlleSeller
+//
+//  Created by Edward Maliniak on 12/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct AlleSellerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ App/AlleSeller/AlleSeller/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AlleSeller
+//
+//  Created by Edward Maliniak on 12/08/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
