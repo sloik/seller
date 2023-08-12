377bc785f5ceed5ed82b856646b233dc83fd95f9
Lukasz Stocki <stocki.lukasz@gmail.com>
2023-08-12 15:34:34 +0200

    Create build-docc.sh

---
+++ App/build-docc.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+## Based on turirial from:
+## https://www.kodeco.com/40047657-docc-tutorial-for-swift-automating-publishing-with-github-actions
+
+# App Scheme
+xcrun xcodebuild docbuild \
+    -project "$PWD/AlleSeller/AlleSeller.xcodeproj" \
+    -scheme AlleSeller \
+    -destination 'generic/platform=iOS Simulator' \
+    -derivedDataPath "$PWD/.derivedData/alleseller"
+
+xcrun docc process-archive transform-for-static-hosting \
+    "$PWD/.derivedData/Build/Products/Debug-iphonesimulator/AlleSeller.doccarchive" \
+    --output-path ".docs" \
+    --hosting-base-path "seller"
+
+# Fixes index file
+echo '<script>window.location.href += "/documentation/alleseller"</script>' > .docs/index.html
+
+
+
+
+
+
+
+
+# Selleer Package
+# xcrun xcodebuild docbuild \
+#     -project ../Seller/Package.swift \
+#     -scheme Seller \
+#     -destination 'generic/platform=iOS Simulator' \
+#     -derivedDataPath "$PWD/.derivedData"
