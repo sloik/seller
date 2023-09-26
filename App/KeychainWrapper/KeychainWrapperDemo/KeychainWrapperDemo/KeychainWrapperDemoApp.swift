bc21bf37d4c008b0aec189c2a818614879aba4be
Lukasz Stocki <stocki.lukasz@gmail.com>
2023-09-26 20:21:00 +0200

    Adds demo app for keychain wrapper.

---
+++ App/KeychainWrapper/KeychainWrapperDemo/KeychainWrapperDemo/KeychainWrapperDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  KeychainWrapperDemoApp.swift
+//  KeychainWrapperDemo
+//
+//  Created by Edward Maliniak on 26/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct KeychainWrapperDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ App/KeychainWrapper/KeychainWrapperDemo/KeychainWrapperDemoUITests/KeychainWrapperDemoUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  KeychainWrapperDemoUITestsLaunchTests.swift
+//  KeychainWrapperDemoUITests
+//
+//  Created by Edward Maliniak on 26/09/2023.
+//
+
+import XCTest
+
+final class KeychainWrapperDemoUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
