39f87a08a9ac5b2122b52979232933f6f2c5044b
Lukasz Stocki <stocki.lukasz@gmail.com>
2023-10-24 22:25:08 +0200

    Removes commented code.

--- App/Seller/Sources/Seller/UI/MySettingsView.swift
+++ App/Seller/Sources/Seller/UI/MySettingsView.swift
@@ -6,16 +6,10 @@ import Lentil
 struct MySettingsView: View {
     @State private var path = NavigationPath()
 
-    // TODO:
-    // if loggedin
-
     @State private var showLoginFlow = false
 
     var body: some View {
         NavigationStack(path: $path) {
-            //            Button {
-            //                showLoginFlow = true
-            //            }
 
             Button("Login") {
                 showLoginFlow = true
