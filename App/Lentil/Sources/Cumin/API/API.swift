7186b1d535c2b1836e4333adcf4bbcf0f26b08b4
Lukasz Stocki <stocki.lukasz@gmail.com>
2023-10-30 22:36:49 +0100

    Adds API client for sandbox.

--- App/Lentil/Sources/Cumin/API/API.swift
+++ App/Lentil/Sources/Cumin/API/API.swift
@@ -15,24 +15,35 @@ private let logger = Logger(subsystem: "API", category: "API")
 
 final class API {
     var _getTokenCode: AsyncThrowsClosure2I<String, APIClientType, Token>
-    var _authClient: APIClientType
+    var _authClient: Closure<Environment, APIClientType>
+    var _environment: Environment
 
     init(
         getTokenCode: @escaping AsyncThrowsClosure2I<String, APIClientType, Token>,
-        authClient: APIClientType
+        authClient:  @escaping Closure<Environment, APIClientType>,
+        environment: Environment
     ) {
         self._getTokenCode = getTokenCode
         self._authClient = authClient
+        self._environment = environment
     }
 }
 
 extension API {
 
+    var currentEnvironment: Environment {
+        _environment
+    }
+
+    var apiClientForCurrentEnvironment: APIClientType {
+        _authClient(_environment)
+    }
+
     func getToken(code: String) async throws -> Token {
         logger.info("Getting token from code: \(code, privacy: .private)")
 
         do {
-            return try await _getTokenCode(code, _authClient)
+            return try await _getTokenCode(code, apiClientForCurrentEnvironment)
         } catch {
             logger.error("Failed to get token from code: \(code, privacy: .private) with error: \(error.localizedDescription)")
             throw error
@@ -47,6 +58,13 @@ extension API {
     }
 }
 
+extension API {
+
+    enum Environment {
+        case prod
+        case sandbox
+    }
+}
 
 
 extension API {
@@ -54,14 +72,16 @@ extension API {
     static var mock: Self {
         .init(
             getTokenCode: { _,_ in Token.mock },
-            authClient: Mock.ApiClient()
+            authClient: Mock.authClient(environment:),
+            environment: .sandbox
         )
     }
 
     static var prod: Self {
         .init(
             getTokenCode: Prod.getToken(code:authClient:),
-            authClient: APIClient(baseURL: URL(string: "https://allegro.pl")!)
+            authClient: Prod.authClient(environment:),
+            environment: .prod
         )
     }
 }
@@ -71,6 +91,15 @@ extension API {
 extension API {
 
     enum Prod {
+
+        static func authClient(environment: Environment) -> APIClientType {
+            switch environment {
+            case .prod:
+                return APIClient(baseURL: URL(string: "https://allegro.pl")!)
+            case .sandbox:
+                return APIClient(baseURL: URL(string: "https://allegro.pl.allegrosandbox.pl")!)
+            }
+        }
 
         static func getToken(code: String, authClient: APIClientType) async throws -> Token {
 
@@ -100,9 +129,14 @@ extension API {
     }
 
     enum Mock {
+
+        static func authClient(environment: Environment) -> APIClientType {
+            Mock.ApiClient()
+        }
+
         final class ApiClient: APIClientType {
 
-            let baseURL: URL = URL(string: "https://allegro.pl")!
+            let baseURL: URL = URL(string: "https://fake.api.pl")!
 
             func get<R>(_ request: R) async throws -> (R.Output, HTTPResponse) where R : Request {
                 fatalError()
