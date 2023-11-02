45ded5e2060242a8921e6dfdca895bedd7e3f537
Lukasz Stocki <stocki.lukasz@gmail.com>
2023-11-02 22:35:26 +0100

    Marks content type as Paginated.

--- App/Lettuce/Sources/Lettuce/MessageCenter/ListMessages/GET/ListMessagesInThread.swift
+++ App/Lettuce/Sources/Lettuce/MessageCenter/ListMessages/GET/ListMessagesInThread.swift
@@ -6,7 +6,7 @@ import HTTPTypes
 import Onion
 
 /// https://developer.allegro.pl/documentation#operation/listMessagesGET
-public struct MessageInThread: Codable, Equatable, ContentType {
+public struct MessageInThread: Codable, Equatable, ContentType, Paginated {
     public let messages: [Message]
     public let offset: Int
     public let limit: Int
