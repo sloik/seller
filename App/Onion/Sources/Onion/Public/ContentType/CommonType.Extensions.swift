4131bcaef900db94ee642624a45076174c55b589
Lukasz Stocki <stocki.lukasz@gmail.com>
2023-11-03 20:52:36 +0100

    Makes `Data` a `ContentType`.

--- App/Onion/Sources/Onion/Public/ContentType/CommonType.Extensions.swift
+++ App/Onion/Sources/Onion/Public/ContentType/CommonType.Extensions.swift
@@ -6,3 +6,5 @@ extension String: ContentType {}
 extension Array: ContentType where Element: ContentType {}
 
 extension Dictionary: ContentType where Key == String, Value: ContentType {}
+
+extension Data: ContentType {}
