c56a2ee3915a7053f114ecd933df75ec68a4b357
Lukasz Stocki <stocki.lukasz@gmail.com>
2023-11-03 18:24:07 +0100

    Adds request.

---
+++ App/Lettuce/Sources/Lettuce/MessageCenter/Threads/PUT/ChangeReadFlagOnThreadRequest.swift
@@ -0,0 +1,49 @@
+// system
+import Foundation
+import HTTPTypes
+
+// local
+import Onion
+
+// https://developer.allegro.pl/documentation#operation/changeReadFlagOnThreadPUT
+struct ChangeReadFlagOnThreadRequest: UploadRequest {
+    typealias Input = Body
+    typealias Output = Response
+    
+    var path: String {
+        "/messaging/threads/\(threadId)/read"
+    }
+
+    var headerFields: HTTPFields {
+        [
+            HTTPField.Name.authorization : "Bearer \(token)",
+            HTTPField.Name.contentType : "application/vnd.allegro.public.v1+json"
+        ]
+    }
+
+    var method: HTTPRequest.Method { .put }
+
+    let body: Body
+    let threadId: String
+    let token: String
+}
+
+extension ChangeReadFlagOnThreadRequest {
+    struct Response: ContentType {
+        let id: String
+        let read: Bool
+        let lastMessageDateTime: String
+
+        struct Interlocutor: ContentType {
+            let login: String
+            let avatarUrl: String
+        }
+        let interlocutor: Interlocutor
+    }
+}
+
+extension ChangeReadFlagOnThreadRequest {
+    struct Body: ContentType {
+        let read: Bool
+    }
+}
---
+++ App/Lettuce/Tests/LettuceTests/ChangeReadFlagOnThreadRequestTests.swift
@@ -0,0 +1,41 @@
+
+import Foundation
+import XCTest
+
+@testable import Lettuce
+
+final class ChangeReadFlagOnThreadRequestTests: XCTestCase {
+
+    func test_shouldParseResponse() {
+
+        // Arrange
+        let jsonString =
+        """
+        {
+          "id": "string",
+          "read": true,
+          "lastMessageDateTime": "2019-08-24T14:15:22Z",
+          "interlocutor": {
+            "login": "string",
+            "avatarUrl": "string"
+          }
+        }
+        """
+
+        let expectedResult = ChangeReadFlagOnThreadRequest.Response(
+            id: "string",
+            read: true,
+            lastMessageDateTime: "2019-08-24T14:15:22Z",
+            interlocutor: ChangeReadFlagOnThreadRequest.Response.Interlocutor(
+                login: "string",
+                avatarUrl: "string"
+            )
+        )
+
+        // Act
+        let result = try! JSONDecoder().decode(ChangeReadFlagOnThreadRequest.Response.self, from: jsonString.data(using: .utf8)!)
+
+        // Assert
+        XCTAssertEqual(result, expectedResult)
+    }
+}
