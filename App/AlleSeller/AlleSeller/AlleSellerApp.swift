b415ac67b13cdd13e5d7b4e3f7e0385ac74698de
Lukasz Stocki <stocki.lukasz@gmail.com>
2023-11-04 23:19:45 +0100

    Uses debug view.

--- App/AlleSeller/AlleSeller/AlleSellerApp.swift
+++ App/AlleSeller/AlleSeller/AlleSellerApp.swift
@@ -3,24 +3,32 @@ import SwiftUI
 
 import Seller
 
+var CurrentSeller: Seller = .init()
+
 @main
 struct AlleSellerApp: App {
 
-    var seller: Seller
+    #if DEBUG
+    @State private var showsDebugView = false
+    #endif
 
     init() {
-        self.seller = .init()
-        self.seller.takeOff()
+        CurrentSeller.takeOff(env: .prod)
     }
 
 
     var body: some Scene {
         WindowGroup {
-            seller
+            CurrentSeller
                 .body
+            #if DEBUG
                 .onShakeGesture {
-                    print(#function)
+                    showsDebugView.toggle()
                 }
+                .sheet(isPresented: $showsDebugView) {
+                    DebugView()
+                }
+            #endif
         }
     }
 }
--- App/Seller/Sources/Seller/Seller.swift
+++ App/Seller/Sources/Seller/Seller.swift
@@ -5,17 +5,31 @@ import OptionalAPI
 
 public final class Seller {
 
-
-
-    public init() {
-        
+    public enum Env {
+        case prod
+        case sandbox
     }
 
-    public func takeOff() {
-        LentilUseCases.start(env: .prod)
+    public init() {
+    }
+
+    public func takeOff(env: Env) {
+        LentilUseCases.start(env: env.lentilEnv)
     }
 
     public var body: some View {
         MainView()
+    }
+}
+
+private extension Seller.Env {
+
+    var lentilEnv: LentilUseCases.Env {
+        switch self {
+        case .prod:
+            return .prod
+        case .sandbox:
+            return .mock
+        }
     }
 }
