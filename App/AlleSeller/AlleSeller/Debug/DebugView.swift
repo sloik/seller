016ba368fe0e013811088b0892ee8f13f7691037
Lukasz Stocki <stocki.lukasz@gmail.com>
2023-11-04 23:19:32 +0100

    Adds debug view

--- App/AlleSeller/AlleSeller/Debug/DebugView.swift
+++ App/AlleSeller/AlleSeller/Debug/DebugView.swift
@@ -4,8 +4,49 @@ import SwiftUI
 import UIKit
 
 struct DebugView: View {
-    
+
+    @Environment(\.dismiss) var dismiss
+
+    @State private var globalNetworkingEnvironment: Bool = true
+
     var body: some View {
-        Text("Debug view")
+
+        NavigationView {
+            List {
+                Section(header: HeaderView(text: "🌐 Global")) {
+                    Text("Item")
+                    Toggle("Networking Environment", isOn: $globalNetworkingEnvironment)
+                        .onChange(of: globalNetworkingEnvironment) { old, new in
+                            print(#function, old, new)
+                        }
+                }
+
+                Section(header: HeaderView(text: "🫐 Lentil")) {
+                    Text("Item")
+                }
+
+                Section(header: HeaderView(text: "🥬 Lettuce")) {
+                    Text("Item")
+                }
+            }
+            .listStyle(InsetGroupedListStyle())
+            .navigationTitle("⚙️ Debug")
+            .toolbar {
+                Button("Dismiss") {
+                    dismiss()
+                }
+            }
+        }
+    }
+}
+
+struct HeaderView: View {
+
+    let text: String
+
+    var body: some View {
+        Text(text)
+            .font(.title3)
+            .bold()
     }
 }
