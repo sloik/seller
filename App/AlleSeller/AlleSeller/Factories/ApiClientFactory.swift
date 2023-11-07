287de202fd706d3e8703df73c0356cfe705aa529
Lukasz Stocki <stocki.lukasz@gmail.com>
2023-11-07 22:31:22 +0100

    Rename the folder so it will not mess up with gitignore.

---
+++ App/AlleSeller/AlleSeller/Factories/ApiClientFactory.swift
@@ -0,0 +1,32 @@
+
+import Foundation
+import Onion
+
+enum ApiClientFactory {
+
+    enum Environment {        
+        case production
+        case sandbox
+        case custom(URL)
+    }
+
+    static func makeApiClient(for environment: Environment) -> APIClientType {
+        APIClient(baseURL: environment.url)
+    }
+}
+
+private extension ApiClientFactory.Environment {
+
+    var url: URL {
+        switch self {
+
+        case .production:
+            return URL(string: "https://allegro.pl")!
+        case .sandbox:
+            return URL(string: "https://allegro.pl.allegrosandbox.pl")!
+        case .custom(let url):
+            return url
+        }
+    }
+}
+
