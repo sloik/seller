026d0fe68d3b2bc8b6c75590cdcb95a3758c92e4
Lukasz Stocki <stocki.lukasz@gmail.com>
2023-11-10 22:54:48 +0100

    Changes the API client used thru the stack and updates UI with that change.

--- App/AlleSeller/AlleSeller/Debug/Global/Networking/NetworkingApiClientChooser.swift
+++ App/AlleSeller/AlleSeller/Debug/Global/Networking/NetworkingApiClientChooser.swift
@@ -7,27 +7,71 @@ import SweetPredicate
 
 struct NetworkingApiClientChooser: View {
 
-    let configurations: [NetworkingConfigurationView.Configuration] = [
-        .init(environment: .production, isActive: false),
-        .init(environment: .sandbox, isActive: false),
-        .init(environment: .custom( URL(string: "www.fake.url")! ), isActive: false)
-    ]
+    @State var configurations: [NetworkingConfigurationView.Configuration] = []
 
     var body: some View {
         List {
-
             ForEach(configurations, id: \.environment) { configuration in
                 NetworkingConfigurationView(configuration: configuration) {
                     let newApiClient = ApiClientFactory.makeApiClient(for: configuration.environment)
                     setCurrent(apiClient: newApiClient)
+                    refreshConfiguration()
                 }
             }
 
+            if configurationContainsCustom.isFalse {
+                Text("TODO: add a way to inject custom URL string")
+            }
         }
         .navigationTitle("🕸️ Networking Client")
+        .onAppear {
+            refreshConfiguration()
+        }
+    }
+
+    private var baseURL: URL {  CurrentSeller.apiClient.baseURL }
+
+    private var environment: ApiClientFactory.Environment {
+        switch baseURL {
+        case ApiClientFactory.Environment.production.url:
+            return .production
+        case ApiClientFactory.Environment.sandbox.url:
+            return .sandbox
+        default:
+            return .custom(baseURL)
+        }
+    }
+
+    var configurationContainsCustom: Bool {
+        configurations
+            .map(\.environment)
+            .contains(where: \.isCustom)
+    }
+
+    private func refreshConfiguration() {
+        configurations = [
+            .init(environment: .production, isActive: environment == .production ),
+            .init(environment: .sandbox, isActive: environment == .sandbox),
+        ]
+
+        configurations
+            .map(\.isActive)
+            .allSatisfy( \.isFalse )
+            .whenTrue {
+                configurations
+                    .append(
+                        .init(
+                            environment: .custom(baseURL),
+                            isActive: true
+                        )
+                    )
+            }
+
     }
 }
 
 private func setCurrent(apiClient: APIClientType) {
-    print("🛤️", #function, apiClient.baseURL.absoluteString)
+    CurrentSeller.configure(
+        using: .init(apiClient: apiClient)
+    )
 }
