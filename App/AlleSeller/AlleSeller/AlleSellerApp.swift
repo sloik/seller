2d8f057cbc374dcc7ad21c1c166fce7b4199a468
Lukasz Stocki <stocki.lukasz@gmail.com>
2023-11-19 16:01:29 +0100

    Displays debug menu in settings for debug builds.

--- App/AlleSeller/AlleSeller/AlleSellerApp.swift
+++ App/AlleSeller/AlleSeller/AlleSellerApp.swift
@@ -36,15 +36,11 @@ struct AlleSellerApp: App {
                 }
             #endif
         }
-        .commands {
-            #if DEBUG
-            CommandMenu("Debug") {
-                Button("Toggle Debug View") {
-                    showsDebugView.toggle()
-                }
-                .keyboardShortcut("d", modifiers: [/*@START_MENU_TOKEN@*/.command/*@END_MENU_TOKEN@*/,.shift])
-            }
-            #endif
+
+        #if os(macOS) && DEBUG
+        Settings {
+            DebugView()
         }
+        #endif
     }
 }
