32c2d45d9c829d9ffb407b1a6b35eac483791abb
Lukasz Stocki <stocki.lukasz@gmail.com>
2023-11-27 21:52:15 +0100

    Adds documentation.

--- App/Utilities/Sources/Utilities/Design/Color/DesignSystem.Color.ShapeStyle.swift
+++ App/Utilities/Sources/Utilities/Design/Color/DesignSystem.Color.ShapeStyle.swift
@@ -3,6 +3,7 @@ import SwiftUI
 
 public extension ShapeStyle where Self == Color {
 
+    /// Applies design system color to shape style.
     static func design(color: DesignSystem.Color, with scheme: ColorScheme) -> Self {
         color.designColor(with: scheme)
     }
--- App/Utilities/Sources/Utilities/Design/Typography/DesignSystem.Typography.View.swift
+++ App/Utilities/Sources/Utilities/Design/Typography/DesignSystem.Typography.View.swift
@@ -3,6 +3,7 @@ import SwiftUI
 
 public extension View {
 
+    /// Applies design system typography to view.
     func design(typography: DesignSystem.Typography) -> some View {
         self.font( typography.designFont )
     }
