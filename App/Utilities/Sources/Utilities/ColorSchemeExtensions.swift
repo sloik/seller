2c475528a1f6257fa6f8d65d29b1cb12c777dd9a
Lukasz Stocki <stocki.lukasz@gmail.com>
2023-11-27 21:54:23 +0100

    Adds case properties.

--- App/Utilities/Sources/Utilities/ColorSchemeExtensions.swift
+++ App/Utilities/Sources/Utilities/ColorSchemeExtensions.swift
@@ -3,6 +3,12 @@ import SwiftUI
 
 public extension ColorScheme {
 
+    /// Returns `true` if the color scheme is dark.
+    var isDark: Bool { self == .dark }
+
+    /// Returns `true` if the color scheme is light.
+    var isLight: Bool { self == .light }
+
     /// Transforms the color scheme into a value of the given type.
     func transform<T>(dark: T, light: T) -> T {
         switch self {
