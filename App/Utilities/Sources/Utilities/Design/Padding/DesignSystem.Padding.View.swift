756e4956b7adc32e691b985331bb7a7ae7871bd5
Lukasz Stocki <stocki.lukasz@gmail.com>
2023-12-03 12:03:51 +0100

    Updates padding definition.

--- App/Utilities/Sources/Utilities/Design/Padding/DesignSystem.Padding.View.swift
+++ App/Utilities/Sources/Utilities/Design/Padding/DesignSystem.Padding.View.swift
@@ -7,4 +7,11 @@ public extension View {
     func design(padding: DesignSystem.Padding) -> some View {
         self.padding(padding.edges, padding.length)
     }
+
+    /// Applies design system padding to view.
+    func design(padding pad1: DesignSystem.Padding, padding pad2: DesignSystem.Padding) -> some View {
+        self
+            .padding(pad1.edges, pad1.length)
+            .padding(pad2.edges, pad2.length)
+    }
 }
--- App/Utilities/Sources/Utilities/Design/Padding/DesignSystem.Padding.swift
+++ App/Utilities/Sources/Utilities/Design/Padding/DesignSystem.Padding.swift
@@ -5,9 +5,26 @@ extension DesignSystem {
 
     public enum Padding {
 
+        /// 6
+        case tiny   (_ edges: Edge.Set)
+        /// 8
         case smaller(_ edges: Edge.Set)
+        /// 12
+        case smal   (_ edges: Edge.Set)
+        /// 16
         case base   (_ edges: Edge.Set)
+        /// 20
+        case big    (_ edges: Edge.Set)
+        /// 24
         case bigger (_ edges: Edge.Set)
+        /// 28
+        case large  (_ edges: Edge.Set)
+        ///32
+        case larger (_ edges: Edge.Set)
+        /// 36
+        case huge   (_ edges: Edge.Set)
+        /// 40
+        case hugger (_ edges: Edge.Set)
 
         @available(*, deprecated, message: "👨‍🎨 Design System: Please use any other value!")
         case custom(edges: Edge.Set, length: CGFloat)
@@ -15,20 +32,36 @@ extension DesignSystem {
 }
 
 extension DesignSystem.Padding {
-    var length: CGFloat {
+    public var length: CGFloat {
         switch self {
-        case .smaller:                  return 12
+        case .tiny:                     return 6
+        case .smaller:                  return 8
+        case .smal:                     return 12
         case .base:                     return 16
-        case .bigger:                   return 20
+        case .big:                      return 20
+        case .bigger:                   return 24
+        case .large:                    return 28
+        case .larger:                   return 32
+        case .huge:                     return 36
+        case .hugger:                   return 40
+
         case .custom(_, let length):    return length
         }
     }
 
-    var edges: Edge.Set {
+    public var edges: Edge.Set {
         switch self {
+        case .tiny(let edges):          return edges
         case .smaller(let edges):       return edges
+        case .smal(let edges):          return edges
         case .base(let edges):          return edges
+        case .big(let edges):           return edges
         case .bigger(let edges):        return edges
+        case .large(let edges):         return edges
+        case .larger(let edges):        return edges
+        case .huge(let edges):          return edges
+        case .hugger(let edges):        return edges
+
         case .custom(let edges, _):     return edges
         }
     }
