46125ae3b4f97b1a7ae288f6e48a85b820d3d632
Lukasz Stocki <stocki.lukasz@gmail.com>
2023-12-04 23:28:40 +0100

    Removes unused code.

--- App/Acorn/Sources/Acorn/UI/LoginView.swift
+++ App/Acorn/Sources/Acorn/UI/LoginView.swift
@@ -8,17 +8,15 @@ struct LoginView: View {
     @Bindable var model: MyAccountViewModel
 
     public var body: some View {
-        NavigationStack(path: $model.path) {
 
-            Button("Login", action: model.didTapLogin)
-                .sheet(isPresented: $model.loginWebViewIsPresented) {
+        Button("Login", action: model.didTapLogin)
+            .sheet(isPresented: $model.loginWebViewIsPresented) {
 
-                    Lentil
-                        .loginUI(didLogin: model.didLogin)
+                Lentil
+                    .loginUI(didLogin: model.didLogin)
 #if os(macOS)
-                        .design(sheet: .regular)
+                    .design(sheet: .regular)
 #endif
-                }
-        }
+            }
     }
 }
