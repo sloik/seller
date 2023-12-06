685bc94d5e69f01d4decb6dd46e4c962ca9f931f
Lukasz Stocki <stocki.lukasz@gmail.com>
2023-12-06 21:28:45 +0100

    Updates unit test.

--- App/AlleSeller/AlleSellerTests/ApiClientFactoryTests.swift
+++ App/AlleSeller/AlleSellerTests/ApiClientFactoryTests.swift
@@ -7,75 +7,11 @@ import Onion
 
 final class ApiClientFactoryTests: XCTestCase {
 
-    func test_environment_name() throws {
-        // Arrange
-        let sut: [ApiClientFactory.Environment] = [
-            .production,
-            .sandbox,
-            .custom(URL(string: "https://example.com")!)
-        ]
-
-        // Act
-        assertInlineSnapshot(
-            of: sut.map(\.name).joined(separator:"\n"),
-            as: .lines
-        ) {
-        """
-        Production
-        Sandbox
-        Custom
-        """
-        }
-    }
-
-    func test_environment_url() throws {
-        // Arrange
-        let sut: [ApiClientFactory.Environment] = [
-            .production,
-            .sandbox,
-            .custom(URL(string: "https://example.com")!)
-        ]
-
-        // Act
-        assertInlineSnapshot(
-            of: sut.map(\.url.absoluteString).joined(separator:"\n"),
-            as: .lines
-        ) {
-        """
-        https://allegro.pl
-        https://allegro.pl.allegrosandbox.pl
-        https://example.com
-        """
-        }
-    }
-
-    func test_environment_isCustom() throws {
-        // Arrange
-        let sut: [ApiClientFactory.Environment] = [
-            .production,
-            .sandbox,
-            .custom(URL(string: "https://example.com")!)
-        ]
-
-        let expectedValues: [Bool] = [
-            false,
-            false,
-            true
-        ]
-
-        // Act
-        let actualValues = sut.map( \.isCustom )
-
-        // Assert
-        XCTAssertEqual(actualValues, expectedValues)
-    }
-
     func test_environment_description() throws {
         // Arrange
-        let sut: [ApiClientFactory.Environment] = [
+        let sut: [AppEnvironment] = [
             .production,
             .sandbox,
-            .custom(URL(string: "https://example.com")!)
         ]
 
         // Act
@@ -86,27 +22,43 @@ final class ApiClientFactoryTests: XCTestCase {
         """
         Production
         Sandbox
-        Custom: https://example.com
         """
         }
     }
 
-    func test_makeApiClient() throws {
+    func test_makeAuthApiClient() throws {
         // Arrange
-        let sut: [ApiClientFactory.Environment] = [
+        let sut: [AppEnvironment] = [
             .production,
             .sandbox,
-            .custom(URL(string: "https://example.com")!)
         ]
 
         let expectedClientURLs = [
             URL(string: "https://allegro.pl")!,
             URL(string: "https://allegro.pl.allegrosandbox.pl")!,
-            URL(string: "https://example.com")!
         ]
 
         // Act
-        let actualValues = sut.map( ApiClientFactory.makeApiClient )
+        let actualValues = sut.map( ApiClientFactory.makeAuthApiClient(for:) )
+
+        // Assert
+        XCTAssertEqual(actualValues.map(\.baseURL), expectedClientURLs)
+    }
+
+    func test_makeRestApiClient() throws {
+        // Arrange
+        let sut: [AppEnvironment] = [
+            .production,
+            .sandbox,
+        ]
+
+        let expectedClientURLs = [
+            URL(string: "https://api.allegro.pl")!,
+            URL(string: "https://api.allegro.pl.allegrosandbox.pl")!,
+        ]
+
+        // Act
+        let actualValues = sut.map( ApiClientFactory.makeRestApiClient(for:) )
 
         // Assert
         XCTAssertEqual(actualValues.map(\.baseURL), expectedClientURLs)
