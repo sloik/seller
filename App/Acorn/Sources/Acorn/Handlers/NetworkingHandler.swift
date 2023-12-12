90971073faf4bf68f9e37f4e3e9009733e45a787
Lukasz Stocki <stocki.lukasz@gmail.com>
2023-12-12 22:21:56 +0100

    WiP: Adds handling of expired token.
    This is only for Acorn. In Lentil this method is still not implemented but the connection is here.

--- App/Acorn/Sources/Acorn/Handlers/NetworkingHandler.swift
+++ App/Acorn/Sources/Acorn/Handlers/NetworkingHandler.swift
@@ -34,7 +34,39 @@ final class NetworkingHandler {
 extension NetworkingHandler: NetworkingHandlerType {
 
     func run<R: Request>(_ request: R) async throws -> (R.Output, HTTPResponse) {
-        try await apiClient.run(request)
+        if request.authorizationWithJWTNeeded {
+             try await tryToRunAndRefreshTokenWhenNeeded(request)
+        } else {
+             try await apiClient.run(request)
+        }
+    }
+}
+
+private extension NetworkingHandler {
+
+    func tryToRunAndRefreshTokenWhenNeeded<R: Request>(_ request: R) async throws -> (R.Output, HTTPResponse) {
+
+        func refreshTokenAndTriggerRequestOneMoreTime() async throws -> (R.Output, HTTPResponse) {
+            logger.debug("Refreshing token and trying to run request \(type(of: R.self))")
+
+            await loginHandler.refreshToken()
+            return try await apiClient.run(request)
+        }
+
+        do {
+            return try await apiClient.run(request)
+        } 
+        // Catch error related expired JWT
+        catch OnionError.notSuccessStatus(let response, _) where response.status.code == 401 {
+            logger.debug("Unauthorised request \(type(of: R.self))")
+
+            return try await refreshTokenAndTriggerRequestOneMoreTime()
+
+        } catch {
+            logger.error("Error running \(type(of: R.self)) with error: \(error.localizedDescription)")
+            throw error
+        }
+
     }
 }
 
