7fd83a2fbe1ee59c07516e8dfd78973b16fb6f50
Lukasz Stocki <stocki.lukasz@gmail.com>
2024-01-25 16:48:06 +0100

    Extracts model to file.

---
+++ App/Lettuce/Sources/Lettuce/ThreadPreviewViewModel.swift
@@ -0,0 +1,54 @@
+
+import Foundation
+
+import Zippy
+
+final class ThreadPreviewViewModel {
+    private let dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .medium
+        formatter.timeStyle = .none
+        formatter.doesRelativeDateFormatting = false
+        return formatter
+    }()
+
+
+    private let relativeDateTimeFormatter: RelativeDateTimeFormatter = {
+        let formatter = RelativeDateTimeFormatter()
+        formatter.dateTimeStyle = .named
+        formatter.unitsStyle = .full
+        return formatter
+    }()
+
+    private let isoDateFormatter: ISO8601DateFormatter = {
+        let formatter = ISO8601DateFormatter()
+        formatter.formatOptions = [.withInternetDateTime, .withFractionalSeconds]
+        return formatter
+    }()
+
+    func lastMessageTime(_ thread: MessageCenterThread) -> (relative: String, time: String)? {
+        Zippy.zip(
+            lastMessageRelativeString(thread),
+            lastMessageString(thread)
+        )
+    }
+}
+
+private extension ThreadPreviewViewModel {
+
+    func lastMessageDate(_ thread: MessageCenterThread) -> Date? {
+        thread
+            .lastMessageDateTime
+            .andThen( isoDateFormatter.date(from:) )
+    }
+
+    func lastMessageString(_ thread: MessageCenterThread) -> String? {
+        lastMessageDate( thread )
+            .andThen( dateFormatter.string(from:) )
+    }
+
+    func lastMessageRelativeString(_ thread: MessageCenterThread) -> String? {
+        lastMessageDate( thread )
+            .andThen { date in relativeDateTimeFormatter.localizedString(for: date, relativeTo: .now) }
+    }
+}
