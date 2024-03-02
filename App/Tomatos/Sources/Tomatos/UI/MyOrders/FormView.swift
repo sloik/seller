dfbdc0dbc1150f99f181aac49403ed6ebb943fb2
Lukasz Stocki <stocki.lukasz@gmail.com>
2024-03-02 12:22:54 +0100

    WiP: layout changes

--- App/Tomatos/Sources/Tomatos/UI/MyOrders/FormView.swift
+++ App/Tomatos/Sources/Tomatos/UI/MyOrders/FormView.swift
@@ -46,34 +46,105 @@ struct FormView: View {
     }
 
     var body: some View {
+        HStack(spacing: 0) {
+            VStack(alignment: .leading, spacing: 0) {
+              "Blok A5 w kropki 60k. klejony z góry, Narcissus Blok A5 w kropki 60k. klejony z góry, Narcissus klejony z góry, Narcissus Blok A5 w kropki 60k. klejony z góry, Narcissus"
+                    .background(Color.green)
+                    .multilineTextAlignment(.leading)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .lineLimit(3)
 
-        HStack {
+                HStack(spacing: 8) {
+
+                    VStack(alignment: .leading, spacing: 0) {
+                        "Klient"
+                            .frame(alignment: .leading)
+
+                        "Otrzymane"
+                            .frame(alignment: .leading)
+
+                        "Zamówienie"
+                            .frame(alignment: .leading)
+
+                    }
+                    .frame(alignment: .leading)
+
+
+                    VStack(alignment: .leading, spacing: 0) {
+                        client
+                            .frame(alignment: .leading)
+
+                        form.updatedAt.or("-")
+                            .frame(alignment: .leading)
+
+                        status
+                            .frame(alignment: .leading)
+
+                    }
+                    .frame(alignment: .leading)
+
+
+                }
+            }
+            .background(Color.yellow)
+             // .padding(.all, 8)
+
+
+            Spacer()
+
+          //  Spacer()
 
             OfferImage(imgURL: offer?.primaryImage.asURL ?? bostonURL)
                 .frame(width: 100, height: 100)
+            //    .padding(.horizontal, 9)
 
-            VStack(alignment: .leading) {
-                offer
-                    .map(\.name)
-                    .or( "NO OFFER NAME" )
-
-                VStack(alignment: .textAlignmentGuide) {
-                    TextAlignedView(
-                        key: "Klient",
-                        value: client
-                    )
-
-                    TextAlignedView(
-                        key: "Otrzymane",
-                        value: form.updatedAt.or("-")
-                    )
-
-                    TextAlignedView(
-                        key: "Zrealizowane",
-                        value: status
-                    )
-                }
-            }
         }
+
+            .background(Color.cyan)
+//.cornerRadius(15)
+
+            //.padding(.all, 24)
+
+        /*.frame(width: UIScreen().bounds.width)*/
     }
 }
+//
+//
+//
+//HStack(spacing: 0) {
+//    OfferImage(imgURL: offer?.primaryImage.asURL ?? bostonURL)
+//        .background(Color.yellow)
+//
+//
+//    VStack(alignment: .leading, spacing: 0) {
+//        "BARDZO DLUGI STRING KTORY NIE WIELE ZNACZY STRING KTORY NIE WIELE ZNACZY STRING KTORY NIE WIELE ZNACZY STRING KTORY NIE WIELE ZNACZY STRING KTORY NIE WIELE ZNACZY"
+//           // .map(\.name)
+//          //  .or( "NO OFFER NAME" )
+//            .lineLimit(3)
+//
+//        VStack(alignment: .textAlignmentGuide) {
+//            TextAlignedView(
+//                key: "Klient",
+//                value: client
+//            )
+//
+//            TextAlignedView(
+//                key: "Otrzymane",
+//                value: form.updatedAt.or("-")
+//            )
+//
+//            TextAlignedView(
+//                key: "Zrealizowane",
+//                value: status
+//            )
+//        }
+//    }
+//    .background(Color.gray)
+//
+//}
+//.background(Color.green)
+//.frame(maxWidth: UIScreen.main.bounds.width - 20)
+//
+//// .frame(width: UIScreen.main.bounds.width)
+//
+//}
