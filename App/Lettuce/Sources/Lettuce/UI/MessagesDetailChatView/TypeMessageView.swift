b6c18420c210cbf1bf3238a22256a252ae400849
Lukasz Stocki <stocki.lukasz@gmail.com>
2024-03-04 22:59:14 +0100

    Extracts some views.

--- App/Lettuce/Sources/Lettuce/UI/MessagesDetailChatView/TypeMessageView.swift
+++ App/Lettuce/Sources/Lettuce/UI/MessagesDetailChatView/TypeMessageView.swift
@@ -1,49 +1,80 @@
 import SwiftUI
 
+import AliasWonderland
+
 internal struct MessageSpacer: View {
-        var body: some View {
-            Spacer()
-                .frame(height: 10)
-        }
+    var body: some View {
+        Spacer()
+            .frame(height: 10)
     }
+}
 
 internal struct TypeMessageView: View {
 
-        @Bindable private var model: MessageDetailChatModel
+    @Bindable private var model: MessageDetailChatModel
 
-        init(model: MessageDetailChatModel) {
-            self.model = model
-        }
-
-        var body: some View {
-            VStack {
-                Divider()
-                HStack(spacing: 0) {
-
-                    Button(action: {
-                        print("Attachment clicked")
-                    }, label: {
-                        Image(systemName: "paperclip")
-                            .design(padding: .large(.horizontal))
-                    })
-
-                    TextField("Type a message", text: $model.conversationMessage, axis: .vertical)
-                        .lineLimit(model.conversationLineLimit)
-                        .design(padding: .large(.leading))
-                    Spacer()
-
-                    Button(action: {
-                        Task {
-                            await model.sendMessageInThread()
-                        }
-                    }, label: {
-                        Image(systemName: "paperplane.fill")
-                            .design(padding: .large(.horizontal))
-                    })
-                }
-                .frame(height: 78)
-                .background(Color.white)
-            }
-        }
+    init(model: MessageDetailChatModel) {
+        self.model = model
     }
 
+    var body: some View {
+        VStack {
+            Divider()
+            HStack(spacing: 0) {
+
+                AddAttachmentView()
+                    .frame(width: 30)
+
+                TextField("Type a message", text: $model.conversationMessage, axis: .vertical)
+                    .lineLimit(model.conversationLineLimit)
+                    .design(padding: .bigger(.leading))
+                    .fixedSize(horizontal: false, vertical: true)
+
+                SendMessageButton {
+                    await model.sendMessageInThread()
+                }
+                .frame(width: 30)
+            }
+            .frame(height: 78)
+            .background(Color.white)
+            .padding()
+        }
+    }
+}
+
+struct AddAttachmentView: View {
+    var body: some View {
+        Menu {
+            Button("Camera") {
+                print("Camera clicked")
+            }
+            Button("Photo Library") {
+                print("Photo Library clicked")
+            }
+            Button("Document") {
+                print("Document clicked")
+            }
+        } label: {
+            Image(systemName: "paperclip")
+                .design(padding: .large(.horizontal))
+        } primaryAction: {
+            print("Run default action...")
+        }
+    }
+}
+
+struct SendMessageButton: View {
+
+    let action: () async -> Void
+
+    var body: some View {
+        Button(action: {
+            Task {
+                await action()
+            }
+        }, label: {
+            Image(systemName: "paperplane.fill")
+                .design(padding: .large(.horizontal))
+        })
+    }
+}
